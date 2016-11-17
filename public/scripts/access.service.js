95f54b750b73213ca8efd1ba2842e63f73a9c770
laserlens <laserlens@gmail.com>
2016-11-17 17:28:40 -0600

    access router and access service to get user's department access

--- public/scripts/access.service.js
+++ public/scripts/access.service.js
@@ -1,2 +1,20 @@
 angular.module('BrandImageManagerApp')
 .service('AccessService', AccessService);
+
+function AccessService ($http) {
+  //do get resquest based on user_id of user querrying the access table
+  this.accesses = function () {
+    return $http({
+      method: 'GET',
+      url: '/access'
+    }).then(function successCallback(response) {
+        console.log('whats the access data', response);
+        return response.data;
+      }, function errorCallback(response) {
+        console.log('Error in Call back');
+      });//end of get
+  };
+  //response data needed would be user_id's boolean value for each department
+
+
+}
