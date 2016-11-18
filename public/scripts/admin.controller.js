e7728de97aa5f45edd13cd972a194d3f1930758d
laserlens <laserlens@gmail.com>
2016-11-18 11:43:36 -0600

    created submissions service and route working on admin controller updated access services and route

--- public/scripts/admin.controller.js
+++ public/scripts/admin.controller.js
@@ -1,7 +1,11 @@
 angular.module('BrandImageManagerApp')
     .controller('AdminController', AdminController);
 
-function AdiminController() {
+function AdiminController($location, AccessService, SubmissionsService) {
     var admin = this;
+
+
+    //call to service to get status of all submissions
+    //push statuses into specific arrays to count number of statuses based on array
 
 }
