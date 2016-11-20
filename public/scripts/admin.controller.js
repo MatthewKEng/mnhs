fd208a62c9c8d79aa3402fb9777c49d0a1f5fb21
laserlens <laserlens@gmail.com>
2016-11-20 12:48:11 -0600

    basic html and ngshow controls for admin page

--- public/scripts/admin.controller.js
+++ public/scripts/admin.controller.js
@@ -4,7 +4,43 @@ angular.module('BrandImageManagerApp')
 function AdminController($location, AccessService, SubmissionsService) {
     var admin = this;
 
+    //ng-show variables onload
+    admin.accessControlsDisplay = false;
+    admin.pendingGalleryDisplay = true;
+    admin.approvedGalleryDisplay = false;
+    admin.revisionGalleryDisplay = false;
 
+    //function to display access
+    admin.showAccess = function () {
+      admin.accessControlsDisplay = true;
+      admin.pendingGalleryDisplay = false;
+      admin.approvedGalleryDisplay = false;
+      admin.revisionGalleryDisplay = false;
+    }
+    //functions to display gallery
+    //display pending gallery
+    admin.showPendingGallery = function () {
+      admin.accessControlsDisplay = false;
+      admin.pendingGalleryDisplay = true;
+      admin.approvedGalleryDisplay = false;
+      admin.revisionGalleryDisplay = false;
+    }
+    //display approved gallery
+    admin.showApprovedGallery = function () {
+      admin.accessControlsDisplay = false;
+      admin.pendingGalleryDisplay = false;
+      admin.approvedGalleryDisplay = true;
+      admin.revisionGalleryDisplay = false;
+      console.log('approved clicked');
+    }
+    //display revision gallery
+    admin.showRevisionGallery = function () {
+      admin.accessControlsDisplay = false;
+      admin.pendingGalleryDisplay = false;
+      admin.approvedGalleryDisplay = false;
+      admin.revisionGalleryDisplay = true;
+      console.log('revision clicked');
+    }
     //call to service to get status of all submissions
     //push statuses into specific arrays to count number of statuses based on array
 
@@ -12,12 +48,6 @@ function AdminController($location, AccessService, SubmissionsService) {
     //modal controlls
     // Get the modal
     var modal = document.getElementById('adminModal');
-
-    // // Get the button that opens the modal
-    // var btn = document.getElementById("myBtn");
-    //
-    // // Get the <span> element that closes the modal
-    // var span = document.getElementsByClassName("close")[0];
 
     // When the user clicks the button, open the modal
     admin.viewButton = function() {
