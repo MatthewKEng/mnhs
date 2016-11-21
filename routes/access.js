69192549e971127205bf113285cc0903569da9ba
laserlens <laserlens@gmail.com>
2016-11-21 11:32:20 -0600

    cleaned up routes access.js

--- routes/access.js
+++ routes/access.js
@@ -15,16 +15,16 @@ router.get('/', function(req, res) {
       done();
       next(error);
     }
-    client.query('SELECT email, admin, alexander_ramsey_house,'+'
-      birch_coulee_battlefield, charles_a_lindbergh_historic_site,'+'
-      comstock_house, folsom_house, fort_ridgely, harkin_store,'+'
-      historic_forestville, historic_fort_snelling, james_j_hill_house,'+'
-      jeffers_petroglyphs, lac_qui_parle_mission, lower_sioux_agency,'+'
-      marine_mill, mill_city_museum, mille_lacs_indian_museum,'+'
-      minnehaha_depot, minnesota_history_center, gale_family_library,'+'
-      minnesota_state_capitol, north_west_company_fur_post, oliver_kelley_farm,'+'
-      sibley_historic_site, split_rock_lighthouse, traverse_des_sioux,'+'
-      w_w_mayo_house from users',function(error, result) {
+    client.query('SELECT email, admin, alexander_ramsey_house,'+
+      'birch_coulee_battlefield, charles_a_lindbergh_historic_site,'+
+      'comstock_house, folsom_house, fort_ridgely, harkin_store,'+
+      'historic_forestville, historic_fort_snelling, james_j_hill_house,'+
+      'jeffers_petroglyphs, lac_qui_parle_mission, lower_sioux_agency,'+
+      'marine_mill, mill_city_museum, mille_lacs_indian_museum,'+
+      'minnehaha_depot, minnesota_history_center, gale_family_library,'+
+      'minnesota_state_capitol, north_west_company_fur_post, oliver_kelley_farm,'+
+      'sibley_historic_site, split_rock_lighthouse, traverse_des_sioux,'+
+      'w_w_mayo_house from users',function(error, result) {
       if (error) {
         done();
         next(error);
