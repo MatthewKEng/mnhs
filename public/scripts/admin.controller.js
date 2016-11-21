804a2fc900d18a0b1a89efe0f89b3e947fec3be8
laserlens <laserlens@gmail.com>
2016-11-21 13:25:39 -0600

    unrounded border radius of navbar and prettied the text for the key in the accordion

--- public/scripts/admin.controller.js
+++ public/scripts/admin.controller.js
@@ -43,7 +43,7 @@ function AdminController($location, AccessService, SubmissionsService) {
     //function for truthy value for access acordion
     admin.truthiness = function (index) {
       //for loop that takes current index of button clicked turns proberty of index true and
-      //all other proberties in the admin.showUserAccess array false
+      //all other properties in the admin.showUserAccess array false
       for (var i = 0; i < admin.allUserAccess.length; i++) {
         if (i == index) {
           admin.showUserAccess[i] = !admin.showUserAccess[i];
@@ -53,7 +53,12 @@ function AdminController($location, AccessService, SubmissionsService) {
            //console.log('whats the truth ',admin.showUserAccess);
         }
       }
+    }
 
+    //make the key pretty function
+    admin.pretty = function (key) {
+      var prettyKey = key.replace(/_/g, " ").toLocaleUpperCase();
+      return prettyKey;
     }
     //call to service to get status of all submissions
     //push statuses into specific arrays to count number of statuses based on array
