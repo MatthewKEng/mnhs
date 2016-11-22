6eac091bf7d7a70550f6e507ea136df7d1a32e29
Meng72 <matthewkeng@hotmail.com>
2016-11-22 09:52:22 -0600

    add end bracket to .col in css

--- public/scripts/gallery.controller.js
+++ public/scripts/gallery.controller.js
@@ -1,15 +1,15 @@
-<<<<<<< HEAD
- angular.module('BrandImageManagerApp')
-      .controller('GalleryController', GalleryController);
 
-      function GalleryController() {
-          console.log('GalleryController loaded');
-          var ctrl = this;
+ // angular.module('BrandImageManagerApp')
+ //      .controller('GalleryController', GalleryController);
+ //
+ //      function GalleryController() {
+ //          console.log('GalleryController loaded');
+ //          var ctrl = this;
+ //
+ //
+ //
+ //        }
 
-
-
-        }
-=======
 // angular.module('BrandImageManagerApp')
 //     .controller('GalleryController', GalleryController);
 
@@ -17,4 +17,3 @@
 //     var gallery = this;
 
 //require login to admin with a true or false statement
->>>>>>> fee0b641ae166c2e47df25a2d89a53e6bf8c4d62
