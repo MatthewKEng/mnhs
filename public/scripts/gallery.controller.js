af188f9f117d3e67eebf6823991ac8e8357dc524
Zach Garman <zachgarman@gmail.com>
2016-11-28 11:43:03 -0600

    starting user sidebar

--- public/scripts/gallery.controller.js
+++ public/scripts/gallery.controller.js
@@ -1,30 +1,11 @@
- //
- //
- // angular.module('BrandImageManagerApp')
- //      .controller('GalleryController', GalleryController);
+angular.module('BrandImageManagerApp')
+       .controller('GalleryController', GalleryController);
 
+function GalleryController(AccessService) {
+  console.log('GalleryController loaded');
+  var ctrl = this;
 
- // angular.module('BrandImageManagerApp')
- //      .controller('GalleryController', GalleryController);
- //
- //      function GalleryController() {
- //          console.log('GalleryController loaded');
- //          var ctrl = this;
- //
- //
- //
- //        }
-
-
-
-
-        // }
-
-
-// angular.module('BrandImageManagerApp')
-//     .controller('GalleryController', GalleryController);
-
-// function AdminController($location, $http) {
-//     var gallery = this;
-
-//require login to admin with a true or false statement
+  ctrl.userDepts = AccessService.userDepts;
+  ctrl.notUserDepts = AccessService.notUserDepts;
+  
+};
