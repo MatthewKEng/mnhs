4f90dd71324eb2304cb34363a9531ea884a4b770
Zach Garman <zachgarman@gmail.com>
2016-11-28 17:02:51 -0600

    pulling adrian's

--- public/scripts/editPhoto.controller.js
+++ public/scripts/editPhoto.controller.js
@@ -107,21 +107,6 @@ function EditPhotoController(Upload, AccessService) {
 
   // Uploads Image to S3 if one is selected.  Also sends image url to SQL db
   // with department_id.
-  photo.uploadPicture = function(form) {
-    if (form.$invalid) {
-      return;
-    }
-    // This only uploads to the first department the user has access to.  Need
-    // to update with the correct Dept ID for the user.
-    var deptName = AccessService.userDepts[0];
-    var deptId = AccessService.departmentIds[deptName];
-    photo.upload.department = deptId;
-    Upload.upload({
-      url: '/image/submission',
-      method: 'POST',
-      data: photo.upload,
-    });
-}
     photo.downloadCanvas = function(canvas) {
         canvas = document.getElementById('canvas');
 
