f435fcce83fa2df0960c066803811442c1ca369c
Meng72 <matthewkeng@hotmail.com>
2016-11-30 09:37:14 -0600

    delete duplicate code in GalleryController

--- public/scripts/gallery.controller.js
+++ public/scripts/gallery.controller.js
@@ -5,31 +5,6 @@ function GalleryController(AuthFactory, SubmissionsService, AccessService, Image
 
   var authFactory = AuthFactory;
 
-
-         console.log('GalleryController loaded');
-         var ctrl = this;
-         ctrl.userDepts = AccessService.userDepts;
-         ctrl.notUserDepts = AccessService.notUserDepts;
-      console.log('username', ctrl.username);
-      console.log('user depts1', ctrl.userDepts);
-      authFactory.isLoggedIn()
-        .then(function(response) {
-            console.log('ctrl controller response ', response);
-            if (response.data.status) {
-                ctrl.displayLogout = true;
-                authFactory.setLoggedIn(true);
-                ctrl.username = response.data.name;
-                console.log('username', ctrl.username);
-            } else { // is not logged in on server
-                ctrl.displayLogout = false;
-                authFactory.setLoggedIn(false);
-            }
-        });
-        ctrl.pretty = function (name) {
-          var prettyUserDept = name.replace(/_/g, " ").toLocaleUpperCase();
-          return prettyUserDept;
-        }
-
   console.log('GalleryController loaded');
   var ctrl = this;
   ctrl.modalImage = {};
