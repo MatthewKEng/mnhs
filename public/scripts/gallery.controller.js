c6dfd304188fd947533d73e91b37dcbcbcc1e99a
laserlens <laserlens@gmail.com>
2016-12-02 01:29:43 -0600

    cleaned up the modal on the gallery page and control user can close modal by clicking anywhere out of modal

--- public/scripts/gallery.controller.js
+++ public/scripts/gallery.controller.js
@@ -24,6 +24,14 @@ function GalleryController($http, AuthFactory, SubmissionsService, AccessService
     ctrl.showDept(ctrl.currentDeptName);
     modal.style.display = "none";
   }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
   // Store current user's access by department
   ctrl.admin = AccessService.admin;
   // If user is an admin, all departments should fall in userDepts
@@ -167,6 +175,7 @@ function GalleryController($http, AuthFactory, SubmissionsService, AccessService
       $timeout(function() {
         ctrl.success = false;
       }, 2500);
+      
     });
   };
 }
