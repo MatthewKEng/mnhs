1c30f9be626ea6d4a8263810fd77bee33273d344
laserlens <laserlens@gmail.com>
2016-12-08 13:05:54 -0600

    created build.sh file

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+mkdir -p public/vendors;
+
+cp node_modules/angular/angular.min.js public/vendors;
+cp node_modules/angular/angular.min.js.map public/vendors;
+
+cp node_modules/bootstrap/dist/css/bootstrap.min.css public/vendors;
+cp node_modules/bootstrap/dist/css/bootstrap.min.css.map public/vendors;
+cp node_modules/bootstrap/dist/fonts/glyphicons-halflings-regular.ttf/vendors;
+
+cp node_modules/angular-route/angular-route.min.js public/vendors;
+cp node_modules/angular-route/angular-route.min.js.map public/vendors;
+
+cp node_modules/ng-file-upload/dist/ng-file-upload.min.js/vendors;
+cp node_modules/ng-file-upload/dist/ng-file-upload-shim.min.js/vendors;
